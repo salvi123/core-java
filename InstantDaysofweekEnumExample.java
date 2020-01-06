49a4e42b98cf7fc37e6ad047be31439ad0643453
SALVISHAH.S <58158623+salvi123@users.noreply.github.com>
2020-01-06 15:49:42 +0530

    java date example

---
+++ InstantDaysofweekEnumExample.java
@@ -0,0 +1,45 @@
+package javadatetime;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+
+public class InstantDaysofweekEnumExample 
+{
+	public static void main(String args[])
+	{
+		Instant inst = Instant.parse("2020-01-03T10:37:30.00Z");
+		System.out.println(inst);
+		Instant instant1 =Instant.now();
+		System.out.println(instant1);
+		Instant instant = Instant.parse("2020-01-03T11:25:30.00Z");
+		instant = instant.minus(Duration.ofDays(125));
+		System.out.println(instant);
+		Instant inst1 =Instant.parse("2020-01-03T11:25:30.00Z");
+		Instant inst2 = inst1.plus(Duration.ofDays(125));
+		System.out.println(inst2);
+		System.out.println(inst.isSupported(ChronoUnit.DAYS));
+		System.out.println(inst.isSupported(ChronoUnit.YEARS));
+		LocalDate localDate = LocalDate.of(2020, Month.JANUARY, 03);
+		DayOfWeek dayofweek =DayOfWeek.from(localDate);
+		System.out.println(dayofweek.get(ChronoField.DAY_OF_WEEK));
+		DayOfWeek day1 =DayOfWeek.of(5);
+		System.out.println(day1.name());
+		System.out.println(day1.ordinal());
+		System.out.println(day1.getValue());
+		LocalDate date =LocalDate.of(2020, Month.JANUARY, 03);
+		DayOfWeek day2=DayOfWeek.from(date);
+		System.out.println(day2.getValue());
+		day2 = day2.plus(3);
+		System.out.println(day2.getValue());
+		day2 = day2.minus(3);
+		System.out.println(day2.getValue());
+		
+		
+	}
+
+}
---
+++ JavaZoneIdExample.java
@@ -0,0 +1,29 @@
+package javadatetime;
+
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class JavaZoneIdExample 
+{
+	public static void main(String args[])
+	{
+		ZoneId zoneid1 = ZoneId.of("Asia/Kolkata");
+		ZoneId zoneid2 = ZoneId.of("Asia/Tokyo");
+		LocalTime id1 = LocalTime.now(zoneid1);
+		LocalTime id2 = LocalTime.now(zoneid2);
+		System.out.println(id1);
+		System.out.println(id2);
+		System.out.println(id1.isBefore(id2));
+		ZoneId zone = ZoneId.systemDefault();
+		System.out.println(zone);
+		String s = zone.getId();
+		System.out.println(s);
+		System.out.println(zone.getDisplayName(TextStyle.FULL, Locale.ROOT));
+		
+		
+		
+	}
+
+}
