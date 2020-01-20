8092231fa70c5d9fa88fc4621d9411f8b3885864
SALVISHAH.S <58158623+salvi123@users.noreply.github.com>
2020-01-20 18:18:36 +0530

    Date and Time Examples

---
+++ JavaSqlDateExample.java
@@ -0,0 +1,18 @@
+package javadateclassical;
+
+import java.sql.Date;
+
+public class JavaSqlDateExample 
+{
+	public static void main(String args[])
+	{
+		long millis=System.currentTimeMillis();
+		java.sql.Date date1=new java.sql.Date(millis);
+		System.out.println(date1);
+		String str="2015-03-31";
+		Date date2=Date.valueOf(str);
+		System.out.println(date2);
+		
+	}
+
+}
